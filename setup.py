45272c7193ce07bf98c83f59c9338226b1f564c9
허용선 <yongseon.heo@gmail.com>
2023-07-20 17:08:56 +0900

    설치 파일 변경

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
 		'Application Framework :: Liquirizia :: EventBroker',
 	],
 	install_requires=[
+		'Liquirizia@git+https://github.com/yong5eon/Liquirizia.git',
 	],
 	python_requires='>=3.8'
 )
