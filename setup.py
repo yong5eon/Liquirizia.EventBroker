9bd6287b1481745ef577361a99ddee398d75bbc2
허용선 <yongseon.heo@gmail.com>
2025-03-28 10:53:21 +0900

    설치 스크립트 추가

---
+++ setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+PKG = 'Liquirizia.EventBroker'
+SRC = 'src'
+EXCLUDES = []
+DESC = 'Event Broker of Liquirizia'
+WHO = 'Heo Yongseon'
+
+PKGS = [PKG]
+DIRS = {PKG: SRC}
+for package in find_packages(SRC, exclude=EXCLUDES):
+	PKGS.append('{}.{}'.format(PKG, package))
+	DIRS['{}.{}'.format(PKG, package)] = '{}/{}'.format(SRC, package.replace('.', '/'))
+
+setup(
+	name=PKG,
+	description=DESC,
+	long_description=open('README.md', encoding='utf-8').read(),
+	long_description_content_type='text/markdown',
+	author=WHO,
+	version=open('VERSION', encoding='utf-8').read(),
+	packages=PKGS,
+	package_dir=DIRS,
+	include_package_data=False,
+	classifiers=[
+		'Programming Language :: Python :: 3.8',
+		'Programming Language :: Python :: 3.9',
+		'Programming Language :: Python :: 3.10',
+		'Programming Language :: Python :: 3.11',
+		'Programming Language :: Python :: 3.12',
+		'Programming Language :: Python :: 3.13',
+		'Liquirizia',
+		'Liquirizia :: EventBroker',
+	],
+	install_requires=[
+		'Liquirizia@git+https://github.com/yong5eon/Liquirizia.git', # TODO : change PyPI
+	],
+	python_requires='>=3.8'
+)
